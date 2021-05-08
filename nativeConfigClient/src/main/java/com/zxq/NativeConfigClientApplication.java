a529395b46798a99c035a1021fb655df7b5f3378
zxq <49315866@qq.com>
2021-05-08 17:23:40 +0800

    本地配置管理

---
+++ nativeConfigClient/src/main/java/com/zxq/NativeConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.zxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxq
+ * @DESCRIPTION
+ * @create 2021/5/8
+ */
+@SpringBootApplication
+public class NativeConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NativeConfigClientApplication.class,args);
+    }
+
+}
