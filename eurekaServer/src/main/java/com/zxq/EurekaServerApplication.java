680612780bdc6e47d3208ceecf94308351b6958b
zxq <49315866@qq.com>
2021-05-08 13:58:54 +0800

    1

---
+++ eurekaServer/src/main/java/com/zxq/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.zxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zxq
+ * @DESCRIPTION
+ * @create 2021/5/8
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+
+}
---
+++ provider/src/main/java/com/zxq/ProviderApplication.java
@@ -0,0 +1,17 @@
+package com.zxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxq
+ * @DESCRIPTION
+ * @create 2021/5/8
+ */
+@SpringBootApplication
+public class ProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class,args);
+    }
+
+}
